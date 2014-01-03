1049844816871a9f70e8070d6893510c33374585
slezier <sylvain.lezier@orange.com>
2014-01-03 14:03:13 +0100

    DisplayContext first drop --SLZ

--- src/test/java/sft/integration/use/DisplayingTestContext.java
+++ src/test/java/sft/integration/use/DisplayingTestContext.java
@@ -1,7 +1,19 @@
 package sft.integration.use;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import sft.SimpleFunctionalTest;
+import sft.integration.fixtures.JUnitHelper;
+import sft.integration.use.sut.DisplayContext;
 
+import java.io.IOException;
+
+@Ignore
+@RunWith(SimpleFunctionalTest.class)
 public class DisplayingTestContext {
     /*
     @Display(AS_STRING)
@@ -11,19 +23,30 @@ public class DisplayingTestContext {
     private Object displayContext2;
      */
 
+    private JUnitHelper functionalTest;
 
     @Test
-    public void displayTestContextWithAnObjectAnnotatedByDisplayable(){
+    public void displayTestContextWithAnObjectAnnotatedByDisplayable() throws IOException {
         allPrivateObjectsAnnotatedByDisplayable();
-        areDisplayedAfterEachFixtureCall();
-    }
-
-    private void areDisplayedAfterEachFixtureCall() {
-        //To change body of created methods use File | Settings | File Templates.
+        areDisplayedAfterEachFixtureCallAndThenUnset();
     }
 
     private void allPrivateObjectsAnnotatedByDisplayable() {
-        //To change body of created methods use File | Settings | File Templates.
+        functionalTest = new JUnitHelper(DisplayContext.class, "target/sft-result/sft/integration/use/sut/DisplayContext.html");
+        functionalTest.run();
     }
+
+    private void areDisplayedAfterEachFixtureCallAndThenUnset() throws IOException {
+        Document html = functionalTest.getHtmlReport();
+
+        Elements select = html.select("*.instruction");
+
+        Assert.assertTrue("Instruction shouldn't display context", select.get(0).select("*.contextDisplayed").isEmpty());
+        Elements expectedContextDisplayed = select.get(1).select("*.contextDisplayed");
+        Assert.assertFalse("Instruction should display context", expectedContextDisplayed.isEmpty());
+        Assert.assertEquals("context displayed", expectedContextDisplayed);
+        Assert.assertTrue("Instruction shouldn't display context", select.get(2).select("*.contextDisplayed").isEmpty());
+    }
+
 
 }
