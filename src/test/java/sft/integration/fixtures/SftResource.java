ae69fc2399de0edc5029407f0f9c6afd56ea5c67
slezier <sylvain.lezier@sfr.fr>
2014-01-20 22:51:23 +0100

    Html do not refresh when opening source window --SLZ

--- src/test/java/sft/integration/fixtures/SftResource.java
+++ src/test/java/sft/integration/fixtures/SftResource.java
@@ -28,7 +28,7 @@ public class SftResource {
 
     public String getOpenResourceHtmlLink(Class aClass, String text, String cssClass) {
         String callerClassHtml = aClass.getSimpleName() + ".html";
-        return "<a href=\"#\" onclick=\"window.open(window.location.href.replace('#','').replace('" + callerClassHtml + "','')+'" + getRelativePathToFile(aClass) + "','" + getHtmlPath() + "','width=800 , menubar=no , status=no , toolbar=no , location=no , resizable=yes , scrollbars=yes',false)\" class=\"badge " + cssClass + "\">" + text + "</a>";
+        return "<a href=\"#\" onclick=\"window.open(window.location.href.replace('#','').replace('" + callerClassHtml + "','')+'" + getRelativePathToFile(aClass) + "','" + getHtmlPath() + "','width=800 , menubar=no , status=no , toolbar=no , location=no , resizable=yes , scrollbars=yes',false);return false;\" class=\"badge " + cssClass + "\">" + text + "</a>";
     }
 
 }
