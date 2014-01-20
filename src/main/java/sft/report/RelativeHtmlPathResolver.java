d9ea86e63df02752fd5eeb52d16f0ff6991c4eed
slezier <sylvain.lezier@orange.com>
2014-01-20 09:18:22 +0100

    add licence --SLZ

--- src/main/java/sft/report/RelativeHtmlPathResolver.java
+++ src/main/java/sft/report/RelativeHtmlPathResolver.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sylvain Lézier.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sylvain Lézier - initial implementation
+ *******************************************************************************/
 package sft.report;
 
 import java.util.LinkedList;
