f87f956381c5dfb891f966858dd3bab021ecc2d0
slezier <sylvain.lezier@orange.com>
2014-03-24 13:45:30 +0100

    Add junit dependency --SLZ

--- src/test/java/bancomat/WithdrawalsTest.java
+++ src/test/java/bancomat/WithdrawalsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class AccountHolderWithdrawCash {
+public class WithdrawalsTest {
 
     private SessionDab atmSession;
     private int withdrawals;
