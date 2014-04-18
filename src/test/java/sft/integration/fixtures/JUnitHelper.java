56abf307e1e3251dbc79d3e1d3d65438ac152196
slezier <sylvain.lezier@gmail.com>
2014-04-18 18:55:02 +0200

    remove JUnitHelper.result

--- src/test/java/sft/integration/fixtures/JUnitHelper.java
+++ src/test/java/sft/integration/fixtures/JUnitHelper.java
@@ -3,7 +3,6 @@ package sft.integration.fixtures;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,17 +10,13 @@ import java.io.IOException;
 public class JUnitHelper {
 
     private final Class functionalTestClass;
-    private final String expectedPathToHtmlResultFile;
     private final Class caller;
-    public final Result result;
     public final Document html;
 
     public JUnitHelper(Class caller,Class functionalTestClass, String expectedPathToHtmlResultFile) throws IOException {
         this.caller = caller;
         this.functionalTestClass = functionalTestClass;
-        this.expectedPathToHtmlResultFile = expectedPathToHtmlResultFile;
-        JUnitCore core = new JUnitCore();
-        result= core.run(functionalTestClass);
+        new JUnitCore().run(functionalTestClass);
         TestFileSystem.filesExists(expectedPathToHtmlResultFile);
         html = Jsoup.parse(new File(expectedPathToHtmlResultFile), "UTF-8", "http://example.com/");
     }
