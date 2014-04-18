3649aff1624ca58b95380f600018d539bf28d9e6
slezier <sylvain.lezier@gmail.com>
2014-04-18 20:10:15 +0200

    Export decorator report generation: first try

---
+++ src/main/java/sft/report/decorators/HtmlBreadcrumb.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2014 Sylvain Lézier.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sylvain Lézier - initial implementation
+ *******************************************************************************/
+package sft.report.decorators;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import sft.DefaultConfiguration;
+import sft.UseCase;
+import sft.decorators.Breadcrumb;
+import sft.decorators.Decorator;
+import sft.report.RelativeHtmlPathResolver;
+import sft.result.FixtureCallResult;
+import sft.result.UseCaseResult;
+
+import java.util.List;
+
+public class HtmlBreadcrumb extends Breadcrumb {
+
+
+    private DefaultConfiguration configuration;
+
+    @Override
+    public Decorator withParameters(String... parameters) {
+        return this;
+    }
+
+    @Override
+    public Decorator withConfiguration(DefaultConfiguration configuration) {
+        this.configuration = configuration;
+        return this;
+    }
+
+    @Override
+    public String applyOnUseCase(UseCaseResult useCaseResult, String result) {
+        final Document parse = Jsoup.parse(result);
+        parse.select(".page-header .text-center").append("<ol class=\"breadcrumb\">" + printFirstUseCase(useCaseResult.useCase,useCaseResult.useCase) + "</ol>");
+        return parse.toString();
+    }
+
+    private String printFirstUseCase(UseCase initialUseCase,UseCase useCaseToBreadcrumb) {
+        String result = "";
+        if (useCaseToBreadcrumb.parent != null) {
+            result = printFirstUseCase(initialUseCase,useCaseToBreadcrumb.parent);
+        }
+        if(initialUseCase == useCaseToBreadcrumb){
+            return result + "<li class=\"active\">" + useCaseToBreadcrumb.getName() + "</li>";
+        }else{
+            final RelativeHtmlPathResolver relativeHtmlPathResolver = configuration.getReport().pathResolver;
+            final String origin = relativeHtmlPathResolver.getPathOf(initialUseCase.classUnderTest, ".html");
+            final String target = relativeHtmlPathResolver.getPathOf(useCaseToBreadcrumb.classUnderTest, ".html");
+            final String pathToUseCaseToBreadcrumb = relativeHtmlPathResolver.getRelativePathToFile(origin, target);
+            return result + "<li><a href=\"" +pathToUseCaseToBreadcrumb+ "\">" + useCaseToBreadcrumb.getName() + "</a></li>";
+        }
+    }
+
+    @Override
+    public String applyOnScenario(String result) {
+        throw new RuntimeException("Breadcrumb can't be apply on scenario");
+    }
+
+    @Override
+    public String applyOnFixtures(List<String> result, List<FixtureCallResult> fixtureCallResuts) {
+        throw new RuntimeException("Breadcrumb can't be apply on scenario");
+    }
+
+
+}
