bbae2433c7fb49de4bf7c5b3bf88861b41a8d0fe
Sylvain Lézier <sylvain.lezier@gmail.com>
2015-02-19 13:43:52 +0100

    Step1

--- sft-tutorial/src/test/java/bancomat/AtmTest.java
+++ sft-tutorial/src/test/java/bancomat/AtmTest.java
@@ -1,32 +1,70 @@
 package bancomat;
 
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
+/*
+As an Account Holder
+I want to withdraw cash from an ATM
+So that I can get money when the bank is closed
+*/
 public class AtmTest {
 
     private SessionDab atmSession;
     private int withdrawals;
+    private Bank bank;
+    private User user;
+    private Atm atm;
+    private Account account;
 
     @Test
-    @Ignore
     public void testWithdrawals() {
-        Bank bank = new Bank();
-        User user = new User();
-        Account account = bank.createAccount(user, 100);
-        account.addValidCreditCard("1234");
-        Atm atm = bank.getAtm(1000);
+        givenTheAccountBalanceIs100Dollars();
+        andTheCardIsValid();
+        andTheMachineContainsEnoughMoney();
 
-        atmSession = atm.authenticate(user);
-        withdrawals = atmSession.withdraw(20);
+        whenTheAccountHolderRequests20Dollars();
 
-        assertEquals(withdrawals, 20);
-        assertEquals(account.balance(), 80);
+        thenTheAtmShouldDispense20Dollars();
+        andTheAccountBalanceShouldBe80Dollars();
+        andTheCardShouldBeReturned();
+    }
+
+
+
+    private void andTheCardShouldBeReturned() {
         assertTrue("Card not returned", atm.returnCard());
     }
+
+    private void andTheAccountBalanceShouldBe80Dollars() {
+        assertEquals(account.balance(), 80);
+    }
+
+    private void andTheCardIsValid() {
+        account.addValidCreditCard("1234");
+    }
+
+    private void givenTheAccountBalanceIs100Dollars() {
+        bank = new Bank();
+        user = new User();
+        account = bank.createAccount(user, 100);
+    }
+
+    private void thenTheAtmShouldDispense20Dollars() {
+        assertEquals(withdrawals, 20);
+    }
+
+    private void whenTheAccountHolderRequests20Dollars() {
+        atmSession = atm.authenticate(user);
+        withdrawals = atmSession.withdraw(20);
+    }
+
+    private void andTheMachineContainsEnoughMoney() {
+        atm = bank.getAtm(1000);
+    }
+
 }
